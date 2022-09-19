243acdbacd6e9a9fec495b9d327b56658fec77de
schweili <111800003+schweili@users.noreply.github.com>
2022-09-18 22:16:11 -0400

    Delete sysinfo.ps1

--- sysinfo.ps1
+++
@@ -1,9 +0,0 @@
-﻿function getIP{
-    (Get-NetIPAddress).IPv4Address | Select-String "192*"
-    }
-$IP = getIP
-$USER = whoami
-$HostName = hostname
-$Version = $PSVersionTable.PSVersion 
-$Date = Get-Date
-Write-Host("This machine's IP is $IP. The User is $USER. The hostname is $HostName. Powershell Version $Version. The date and time is $Date")
