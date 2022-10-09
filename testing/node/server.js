dd1ecf14044a45e3155cda7eff2aa36434b709d1
schweili <111800003+schweili@users.noreply.github.com>
2022-10-09 19:52:55 -0400

    added server.js
    had to do it this way because VM was acting up .

---
+++ testing/node/server.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+const os = require("os");
+const ip = require('ip');
+
+http.createServer((req, res) => {
+  if (req.url === "/") {
+      fs.readFile("./it3038c-scripts/index.html", "UTF-8", (err, body) => {
+      res.writeHead(200, {"Content-Type": "text/html"});
+      res.end(body);
+    });
+  } else if(req.url.match("/sysinfo")) {
+    myHostName=os.hostname();
+    html=`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Node JS Response</title>
+      </head>
+      <body>
+        <p>Hostname: ${myHostName}</p>
+        <p>IP: ${ip.address()}</p>
+        <p>Server Uptime: </p>
+        <p>Total Memory: </p>
+        <p>Free Memory: </p>
+        <p>Number of CPUs: </p>
+      </body>
+    </html>`
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.end(html);
+  } else {
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.end(`404 File Not Found at ${req.url}`);
+  }
+}).listen(3000);
+
+console.log("Server listening on port 3000");
